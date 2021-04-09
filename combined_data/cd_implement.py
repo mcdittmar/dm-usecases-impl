9c40310395b3694d1c5948fdb098cd4ba5e18d75
mcdittmar <mdittmar@cfa.harvard.edu>
2021-04-09 14:55:11 -0400

    tweak markdown for proper rendering

--- combined_data/cd_implement.py
+++ combined_data/cd_implement.py
@@ -38,29 +38,30 @@ def xmm_summary( infile ):
     fh.write("Input file: {}\n".format(os.path.basename(infile)))
     fh.write("\n")
     
-    fh.write("Goal: Find Source-s\n")
-    fh.write("'''\n")
-    fh.write("doc = Reader( Votable(infile) )\n")
-    fh.write("catalog = doc.find_instances(Source)[0]\n")
-    fh.write("'''\n")
+    fh.write("### Goal: Find Source-s\n")
+    fh.write("    doc = Reader( Votable(infile) )\n")
+    fh.write("    catalog = doc.find_instances(Source)[0]\n")
 
     doc = Reader( Votable(infile) )
     catalog = doc.find_instances(Source)[0]
 
     fh.write("\n")
-    fh.write("Goal: High Level content summary\n")
+    fh.write("### Goal: High Level content summary\n")
+    fh.write("```\n")
     fh.write("Source List\n")
     write_catalog_instance_summary(fh, catalog )
+    fh.write("```\n")
 
     fh.write("\n")
-    fh.write("Goal: Source record detail\n")
-    fh.write("'''\n")
-    fh.write("for source in catalog.unroll():\n")
-    fh.write("    print( source_toString( source ) )\n")
-    fh.write("'''\n")
-    
+    fh.write("### Goal: Source record detail\n")
+    fh.write("    for source in catalog.unroll():  \n")
+    fh.write("        print( source_toString( source ) )\n")
+    fh.write("\n")
+    fh.write("results in\n")
+    fh.write("```\n")
     for source in catalog.unroll():
         fh.write( source_toString( source ) )
+    fh.write("```\n")
     
     fh.close()
 
@@ -74,13 +75,11 @@ def csc_summary( infile ):
     fh.write("Input file: {}\n".format(os.path.basename(infile)))
     fh.write("\n")
     
-    fh.write("Goal: Find Primary Instances\n")
-    fh.write("'''\n")
-    fh.write("doc = Reader( Votable(infile) )\n")
-    fh.write("masterSources = doc.find_instances(Source)[0]\n")
-    fh.write("detections = doc.find_instances(Source)[1]\n")
-    fh.write("lightcurves = doc.find_instances(SparseCube)[0]\n")
-    fh.write("'''\n")
+    fh.write("### Goal: Find Primary Instances\n")
+    fh.write("    doc = Reader( Votable(infile) )\n")
+    fh.write("    masterSources = doc.find_instances(Source)[0]\n")
+    fh.write("    detections = doc.find_instances(Source)[1]\n")
+    fh.write("    lightcurves = doc.find_instances(SparseCube)[0]\n")
     
     doc = Reader( Votable(infile) )
     allsources = doc.find_instances(Source)
@@ -89,7 +88,8 @@ def csc_summary( infile ):
     lightcurves = doc.find_instances(SparseCube)[0]
     
     fh.write("\n")
-    fh.write("Goal: High Level content summary\n")
+    fh.write("### Goal: High Level content summary\n")
+    fh.write("```\n")
     fh.write("Master Source List\n")
     write_catalog_instance_summary( fh, master )
 
@@ -100,34 +100,34 @@ def csc_summary( infile ):
     fh.write("\n")
     fh.write("LightCurves - one per source\n")
     write_lightcurve_instance_summary( fh, lightcurves )
+    fh.write("```\n")
 
     fh.write("\n")
-    fh.write("Goal: Detailed content example\n")
+    fh.write("### Goal: Detailed content example\n")
     master_sources = master.unroll()
     srcno = 3
     source = master_sources[srcno]
     fh.write("Source number: %d\n"%( srcno+1 ) )
+    fh.write("```\n")
     fh.write( source_toString( source ) )
-
-    fh.write("\n")
-    fh.write("Done\n")
+    fh.write("```\n")
     
     fh.close()
 
 def write_catalog_instance_summary( fh, catalog ):
-    fh.write("o Type: %s\n"%(get_type_name( catalog )))
-    fh.write("o Number of records: %d\n"%( len(catalog.identifier) ))
-    fh.write("o Number of unique Sources: %d\n"%( len(set(catalog.identifier)) ))
-    fh.write("o Associated Parameters: %s\n"%( "none" if catalog.parameter_dock is None else str(len(catalog.parameter_dock))))
-    fh.write("o Associated Data: %s\n"%( "none" if catalog.associated_data_dock is None else str(len(catalog.associated_data_dock))))
+    fh.write("  o Type: %s\n"%(get_type_name( catalog )))
+    fh.write("  o Number of records: %d\n"%( len(catalog.identifier) ))
+    fh.write("  o Number of unique Sources: %d\n"%( len(set(catalog.identifier)) ))
+    fh.write("  o Associated Parameters: %s\n"%( "none" if catalog.parameter_dock is None else str(len(catalog.parameter_dock))))
+    fh.write("  o Associated Data: %s\n"%( "none" if catalog.associated_data_dock is None else str(len(catalog.associated_data_dock))))
 
 def write_lightcurve_instance_summary( fh, lightcurve ):
     lcpoints = lightcurve.data
-    fh.write("o Type: {}\n".format(get_type_name(lightcurve)))
-    fh.write("o Number of curves: {}\n".format(len(lcpoints[0])))
-    fh.write("o Independent Axis: {}\n".format(str(lcpoints[0][0].independent)))
-    fh.write("o Dependent Axis: {}\n".format(str(lcpoints[0][0].dependent)))
-    fh.write("o Length (max detections): {}\n".format(len(lcpoints)))
+    fh.write("  o Type: {}\n".format(get_type_name(lightcurve)))
+    fh.write("  o Number of curves: {}\n".format(len(lcpoints[0])))
+    fh.write("  o Independent Axis: {}\n".format(str(lcpoints[0][0].independent)))
+    fh.write("  o Dependent Axis: {}\n".format(str(lcpoints[0][0].dependent)))
+    fh.write("  o Length (max detections): {}\n".format(len(lcpoints)))
     
 if __name__=="__main__":
     try:
